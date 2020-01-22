da380e7122a576caae04ae53fc3ae936d45f2fa7
fedgut <fedgut@gmail.com>
2020-01-22 11:42:47 -0600

    Create user and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/creaete'
+  get 'users/new'
+  get 'user/new'
+  get 'user/create'
+  get 'user/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
