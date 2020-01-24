95cfcd058dd9f6495bbdb50bfdbe2116af34ef1a
fedgut <fedgut@gmail.com>
2020-01-24 10:18:25 -0600

    drop password from users table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  has_many :events, foreign_key: 'creator_id', class_name: 'Event'
+  has_many :user_events
+  has_many :attended_events, through: :user_events, source: :event
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_160623) do
+ActiveRecord::Schema.define(version: 2020_01_24_161557) do
 
   create_table "events", force: :cascade do |t|
     t.date "date"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2020_01_24_160623) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
