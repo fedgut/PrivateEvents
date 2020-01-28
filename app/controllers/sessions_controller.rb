b7b2bede960a2929c6744c61f72a7a1ade5c2735
fedgut <fedgut@gmail.com>
2020-01-28 12:02:19 -0600

    fix lint warnings

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    @user = (User.find_by(email: params[:email].downcase) unless params[:email]&.empty?)
+    @user = (User.find_by(email: params[:email].downcase) unless params[:email].empty?)
 
     if @user
       log_in(@user)
